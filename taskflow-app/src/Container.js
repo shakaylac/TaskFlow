a2d0201689f0f20c8510de3ca70d6e744ad28a85
shakayla <kaycnite@gmail.com>
2025-06-30 20:38:12 -0500

    Added rectangle elements to create glassmorphic look for  background. Can't find the rec-1 element to realign it. Added margin-left and margin-top tp rec-2. align-self,justify-self, align-items, and justify-content did not wotk for their respective elements.

--- taskflow-app/src/Container.js
+++ taskflow-app/src/Container.js
@@ -3,6 +3,8 @@ import './CSS/Container.css';
 function Container() {
     return(
         <>
+        <div className="rec-1"></div>
+        <div className="rec-2"></div>
         <div className="container"></div>
         </>
     );
