0b95f31b3a1d944e38070b9793827780709f2722
shakayla <kaycnite@gmail.com>
2025-07-01 20:46:49 -0500

    Completed glassmorpic backgorund. Added a lamp div as the other elements for the backgound wouldn't show up withing something else being there. Like the second rec wouldn't show up until I added the oval element. So there's a lamp there now and if it's not there the background won't have all the elements it needs.

--- taskflow-app/src/Container.js
+++ taskflow-app/src/Container.js
@@ -3,8 +3,11 @@ import './CSS/Container.css';
 function Container() {
     return(
         <>
+        <div className="lamp"></div>
+        <div className="oval"></div>
         <div className="rec-1"></div>
         <div className="rec-2"></div>
+        <div className="rec-3"></div>
         <div className="container"></div>
         </>
     );
