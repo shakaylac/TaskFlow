96ad2fe6756ff024e659518f792e4c0f13add427
shakayla <kaycnite@gmail.com>
2025-07-04 21:34:09 -0500

    Added Calendar html. Mainly, worked with the previous, next, month. Added images for the previou and next buttons and did some javascript with useEffect and useState to display the current month. It didn't work, I'll still have to iron out the kinks. I put the useState for the month as June for styling. Looking to give teh btns no border. border: none; did not do what I needed it to do. I'll look into that later.

--- taskflow-app/src/Calendar.js
+++ taskflow-app/src/Calendar.js
@@ -1,8 +1,43 @@
 import './CSS/Calendar.css';
+import React, { useEffect, useState } from 'react';
+import arrow_right from'./assets/arrow_right.svg';
+import arrow_left from'./assets/arrow_left.svg';
 
 function Calendar() {
+
+    const [month, setMonth] = useState('June');
+
+    useEffect(() => {
+        const date = new Date;
+        const currentmonth = date.getMonth();
+
+        const monthNames = [
+            "Janurary", "Februray", "March", "April", "May", "June",
+            "July", "August", "September", "November", "Decemeber"
+        ];
+
+        const currentmonthName = monthNames[date.getMonth()];
+
+        if(currentmonthName === currentmonth) {
+            setMonth(currentmonthName);
+        };
+
+
+    }, []);
+
+
     return(
         <section className='calendar-com'>
+            <span className='calendar-nav'>
+                <button className='previous-btn' id='previous-btn'><img src={arrow_left} className='arrow-left'></img></button>
+                <p className='month' style={{fontFamily: 'Inter', color: '#696868', fontSize: '15px', fontWeight: 'bold' }}> {month} </p>
+                <button className='next-btn' id='next-btn'><img src={arrow_right} className='arrow-right'></img></button>
+            </span>
+
+            <div className='days'>
+
+            </div>
+
 
         </section>
 
