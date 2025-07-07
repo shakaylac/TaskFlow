685a8eb322a8f5b46f8ca5263aade0cfa0bb1a4e
shakayla <kaycnite@gmail.com>
2025-07-07 08:19:00 -0500

    I added the days of the week and styled them. The ol has a margin-left of -50px because it was too far to the right and centering the calendar-com didn't do anything. I also gave them a margin-top of 5px becasue it was too low and wouldn't give enough space for the days.

--- taskflow-app/src/Calendar.js
+++ taskflow-app/src/Calendar.js
@@ -6,9 +6,15 @@ import arrow_left from'./assets/arrow_left.svg';
 function Calendar() {
 
     const [month, setMonth] = useState('');
+    const [prevMonthbtn, setPrevMonthBtn] = useState('');
+    const [nextMonthbtn, setNextMonthBtn] = useState('');
+    const [weekdays, setWeekdays] = useState('');
+    const [days, setDays] = useState('');
 
     useEffect(() => {
         const date = new Date();
+        let currentMonth = date.getMonth();
+        let currentYear = date.getFullYear();
 
         const monthNames = [
             "January", "February", "March", "April", "May", "June",
@@ -17,21 +23,42 @@ function Calendar() {
 
         const currentmonthName = monthNames[date.getMonth()];
         setMonth(currentmonthName);
+        
+
 
     }, []);
 
+   
 
     return(
         <section className='calendar-com'>
             <span className='calendar-nav'>
                 <button className='previous-btn' id='previous-btn'><img src={arrow_left} className='arrow-left'></img></button>
-                <p className='month' style={{fontFamily: 'Inter', color: '#696868', fontSize: '15px', fontWeight: 'bold' }}> {month} </p>
+                <p className= 'month' style={{fontFamily: 'Inter', color: '#696868', fontSize: '15px', fontWeight: 'bold' }}> {month} </p>
                 <button className='next-btn' id='next-btn'><img src={arrow_right} className='arrow-right'></img></button>
+                
+                
+
+
+                <div className='calendar-body'>
+                    <ol className='weekdays'>
+                        <li>Sun</li>
+                        <li>Mon</li>
+                        <li>Tue</li>
+                        <li>Wed</li>
+                        <li>Thu</li>
+                        <li>Fri</li>
+                        <li>Sat</li>
+                    </ol>
+                    <div className='days'>
+
+                    </div>
+
+                 </div>
+
             </span>
 
-            <div className='days'>
-
-            </div>
+    
 
 
         </section>
