4effbad1304acd0e7b6c337da6fa0dae22e5e297
shakayla <kaycnite@gmail.com>
2025-07-27 21:29:29 -0500

    I have spent my time placing div sin other divs. I placed the h5 headers inside of their respective columns and created cards for the first column. I'll need to find a quick way to edit the red, orange, ane green cards that show priorty and I'm already thinking of how I can make them customizable using javascript. I'm excited by exhausted.

--- taskflow-app/src/Pages/Kanban.js
+++ taskflow-app/src/Pages/Kanban.js
@@ -4,10 +4,27 @@ function Kanban() {
     return(
         <div className="kanban-container">
             <div className="kanban-headers">
-                <h5 className="todo-header">To Do</h5>
-                <h5 className="in-progress-header">In Progress</h5>     
-                <h5 className="review-header">Review</h5>       
-                <h5 className="complete-header">Complete</h5>
+                     <div className="todo-column">
+                        <h5 className="todo-header">To Do</h5>
+                        <div className="todo-cards">
+                            <div className="todo-card">Task 1</div>
+                            <div className="todo-card">Task 2</div>
+                            <div className="todo-card">Task 3</div>
+                            <div className="todo-card">Task 4</div>
+                        </div>
+                    </div>
+                        
+                    <div className="in-progress-column">
+                        <h5 className="in-progress-header">In Progress</h5> 
+                    </div>    
+
+                    <div className="review-column">
+                        <h5 className="review-header">Review</h5> 
+                    </div>      
+
+                    <div className="complete-column">
+                        <h5 className="complete-header">Complete</h5>
+                    </div>
             </div>
 
         </div>
