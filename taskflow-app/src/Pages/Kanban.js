221283217731f26ecab7f21ad62b6c4ffbc1e7b9
shakayla <kaycnite@gmail.com>
2025-07-28 12:33:28 -0500

    Added column specific cards. Red for high prrioty, orange for midium priorty, and green for low priorty. I'm going back and forth on whether to keep blue for completeed or to stick to the design on Figma and leave them white. I want to implement a feat. that will change the priorty color based on time. Like a week before a project is due the priority will change to red/high.

--- taskflow-app/src/Pages/Kanban.js
+++ taskflow-app/src/Pages/Kanban.js
@@ -7,23 +7,41 @@ function Kanban() {
                      <div className="todo-column">
                         <h5 className="todo-header">To Do</h5>
                         <div className="todo-cards">
-                            <div className="todo-card">Task 1</div>
-                            <div className="todo-card">Task 2</div>
-                            <div className="todo-card">Task 3</div>
-                            <div className="todo-card">Task 4</div>
+                            <div className="red-card">Task 1</div>
+                            <div className="orange-card">Task 2</div>
+                            <div className="green-card">Task 3</div>
+                            <div className="orange-card">Task 4</div>
                         </div>
                     </div>
                         
                     <div className="in-progress-column">
                         <h5 className="in-progress-header">In Progress</h5> 
+                        <div className="in-progress-cards">
+                            <div className="red-card">Task 1</div>
+                            <div className="orange-card">Task 2</div>
+                            <div className="green-card">Task 3</div>
+                        </div>
                     </div>    
 
                     <div className="review-column">
                         <h5 className="review-header">Review</h5> 
+                        <div className="review-cards">
+                            <div className="orange-card">Task 1</div>
+                            <div className="green-card">Task 2</div>
+                        </div>
                     </div>      
 
                     <div className="complete-column">
                         <h5 className="complete-header">Complete</h5>
+                        <div className="complete-cards">
+                            <div className="complete-card">Task 1</div>
+                            <div className="complete-card">Task 2</div>
+                            <div className="complete-card">Task 3</div>
+                            <div className="complete-card">Task 4</div>
+                            <div className="complete-card">Task 5</div>
+                            <div className="complete-card">Task 6</div>
+                            <div className="complete-card">Task 7</div>
+                        </div>
                     </div>
             </div>
 
