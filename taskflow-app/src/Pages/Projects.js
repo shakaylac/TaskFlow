08620d417b0e17eb2b13115ac86b73548452c6df
shakaylac <kaycnite@gmail.com>
2025-08-28 20:56:21 -0500

    Progress bar and Icons were added. Needed to create new divs to have everyting align correctly. Deleted 45% height on project-cards so the wrapping would work.

--- taskflow-app/src/Pages/Projects.js
+++ taskflow-app/src/Pages/Projects.js
@@ -2,10 +2,71 @@ import '../CSS/Project.css';
 
 
 function Projects() {
-    return(
-        <div className="projects-container">
+    return( 
+        <>
+        <div className='Projects'>
+            <div className="projects-container">
+                <div className='project-cards'>
+                    <div id='cora-design' className='cora-design'> 
+                        <div className="project-header">
+                            <p>Cora Design </p>
+                            <div className="members">
+                                <div className="circle-1"></div>
+                                <div className="circle-2"></div>
+                                <div className="circle-3"></div>
+                            </div>
+                        </div>
+                        <div className="progress-bar">
+                            <div className="progress"></div>
+                        </div>
+                    </div>
+                    <div id='cora-marketing' className='cora-marketing'> 
+                            <div className="project-header">
+                                <p>Cora Marketing</p>
+                                <div className="members">
+                                    <div className="circle-1"></div>
+                                    <div className="circle-2"></div>
+                                    <div className="circle-3"></div>
+                                </div>
+                            </div>
+                        <div className="progress-bar">
+                            <div className="progress"></div>
+                        </div>
+                    </div>
+                    <div id='cora-research' className='cora-research'> 
+                        <div className="project-header">
+                            <p>Cora Research</p>
+                            <div className="members">
+                                <div className="circle-1"></div>
+                                <div className="circle-2"></div>
+                                <div className="circle-3"></div>
+                            </div>
+                        </div>
+                        <div className="progress-bar">
+                            <div className="progress"></div>
+                        </div>
+                    </div>
+                </div>
 
+                <div className='project-stats'>
+                    <div className='link-container'> 
+                        <div className='link-header'></div>
+                        <div className='link-body'></div>
+
+                    </div>
+
+                    <div className='inbox-container'>
+
+                    </div>
+                
+                </div>
+
+            </div>
+
+           
         </div>
+
+        </>
 
     )
 }
