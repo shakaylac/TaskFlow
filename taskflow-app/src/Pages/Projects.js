0aa1e79bdabb7c559cb922872f6c4fc3b63a6397
shakaylac <kaycnite@gmail.com>
2025-08-31 15:58:59 -0500

    Edit and delete icons added. Completed inbox session.

--- taskflow-app/src/Pages/Projects.js
+++ taskflow-app/src/Pages/Projects.js
@@ -1,4 +1,6 @@
 import '../CSS/Project.css';
+import editIcon from '../assets/edit.svg';
+import deleteIcon from '../assets/delete.svg';
 
 
 function Projects() {
@@ -75,19 +77,63 @@ function Projects() {
                         <div className='link-body'></div>
                             <div className='link-item'>
                                 <a href="#">StyleSheet_Mayden.pdf</a>
+                                <button className='edit'><img src={editIcon} /></button>
+                                <button className='delete'><img src={deleteIcon} /></button>
                             </div>
                             <div className='link-item'>
                                 <a href="#">Brief_Mayden.pdf</a>
+                                <button className='edit'><img src={editIcon} /></button>
+                                <button className='delete'><img src={deleteIcon} /></button>
                             </div>
                             <div className='link-item'>
                                 <a href="#">Presentation_Mayden.pdf</a>
+                                <button className='edit'><img src={editIcon} /></button>
+                                <button className='delete'><img src={deleteIcon} /></button>
                             </div>
                             
 
                     </div>
 
                     <div className='inbox-container'>
-
+                        <div className='inbox-header'>
+                            <p>Recent Conversations</p>
+                            <button className="view-all-btn">View All</button>
+                        </div>
+                        <div className='inbox-body'>
+                            <div className='inbox-item'>
+                                <div className='inbox-message-header'>
+                                    <div className='inbox-circle-blue'>RM</div>
+                                    <p className='Title'> Brainstorm Session </p>
+                                    <div className='inbox-number'>4</div>
+                                </div>
+                                <div className='inbox-text'>
+                                     <span>2 hrs ago</span>
+                                    <p className='message'>John Cattens: completed task "Design Landing Page"</p>
+                                </div>
+                            </div>
+                            <div className='inbox-item'>
+                                <div className='inbox-message-header'>
+                                    <div className='inbox-circle-yellow'>BS</div>
+                                    <p className='Title'> Marketing Sync </p>
+                                    <div className='inbox-number'>2</div>
+                                </div>
+                                <div className='inbox-text'>
+                                    <span>5 hrs ago</span>
+                                    <p className='message'>Edgar Poe: commented on task "Market Research"</p>
+                                </div>
+                            </div>
+                            <div className='inbox-item'>
+                                <div className='inbox-message-header'>
+                                    <div className='inbox-circle-purple'>AJ</div>
+                                    <p className='Title'> Survey Findings </p>
+                                    <div className='inbox-number'>9</div>
+                                </div>
+                                <div className='inbox-text'>
+                                    <span>1 day ago</span>
+                                    <p className='message'>Alexis Jenkins: added you to project "Cora Research"</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 
                 </div>
